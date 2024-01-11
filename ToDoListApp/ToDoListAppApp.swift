601589f177b5ae4fda04742cf0daf0edf0d46423
soumyaranjanmohapatra <srmohapatra421@gmail.com>
2024-01-11 20:45:04 +0530

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Soumya Ranjan Mohapatra on 11/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
